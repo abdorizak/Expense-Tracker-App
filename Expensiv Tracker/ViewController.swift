624b7f39adf8d33f8c04bf4cb38e5cc890489ad4
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-02-28 16:29:18 +0300

    Initial Commit

---
+++ Expensiv Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expensiv Tracker
+//
+//  Created by Abdirizak Hassan on 2/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
