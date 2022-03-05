f34a6e7b40546bb94fc1ec772cf43d3358497ea8
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-05 13:35:35 +0300

    Controller - Main - AddExpensesVC

--- Expensiv Tracker/Controllers/Main/AddExpensesVC.swift
+++ Expensiv Tracker/Controllers/Main/AddExpensesVC.swift
@@ -11,19 +11,10 @@ class AddExpensesVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
