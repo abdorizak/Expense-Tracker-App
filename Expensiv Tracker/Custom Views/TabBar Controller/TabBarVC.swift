620a41f007eb4b9314bd02bd72967759157d4a2a
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-05 14:06:15 +0300

    CustomView - TabBar Controller

--- Expensiv Tracker/Custom Views/TabBar Controller/TabBarVC.swift
+++ Expensiv Tracker/Custom Views/TabBar Controller/TabBarVC.swift
@@ -16,17 +16,26 @@ class TabBarVC: UITabBarController {
     
     private func configureTabBar() {
         view.backgroundColor = .systemBackground
-        viewControllers = [configHomeVC()]
+        viewControllers = [configHomeVC(), addExpensesVC(), ]
     }
     
     private func configHomeVC() -> UINavigationController {
         let homeVC = HomeVC()
-        homeVC.tabBarItem = UITabBarItem(title: "house.fill", image: UIImage(systemName: ""), tag: 0)
+        homeVC.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "house.fill"), tag: 0)
         return UINavigationController(rootViewController: homeVC)
     }
     
-    private func addExpensesVC() -> UINavigationController {
-        let vc
+    private func addExpensesVC() -> UINavigationController  {
+        let addexpensesVC = AddExpensesVC()
+        addexpensesVC.modalPresentationStyle = .overCurrentContext
+        addexpensesVC.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "plus.circle.fill"), tag: 1)
+        return UINavigationController(rootViewController: addexpensesVC)
+    }
+    
+    private func transectionVC() -> UINavigationController {
+        let tranVC = TransectionsVC()
+        tranVC.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "wave.fill"), tag: 2)
+        return UINavigationController(rootViewController: tranVC)
         
     }
 
