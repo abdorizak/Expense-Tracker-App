07ef392595a787192606fec064f11f7fdf4fb664
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-05 13:35:58 +0300

    Controller - Main - TransectionsVC

--- Expensiv Tracker/Controllers/Main/TransectionsVC.swift
+++ Expensiv Tracker/Controllers/Main/TransectionsVC.swift
@@ -11,19 +11,13 @@ class TransectionsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "Tractions"
+        navigationItem.largeTitleDisplayMode  = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
