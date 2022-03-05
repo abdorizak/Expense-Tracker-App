8aad8a2a343cdb270af2c4875c8084ef46e7ae3f
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-05 13:34:00 +0300

    Controllers - ForgetPasswordVC

--- Expensiv Tracker/Controllers/Forget Password/ForgetPasswordVC.swift
+++ Expensiv Tracker/Controllers/Forget Password/ForgetPasswordVC.swift
@@ -8,18 +8,47 @@
 import UIKit
 
 class ForgetPasswordVC: UIViewController {
+    
+    
+    let forgetPasswordImage = UIImageView()
+    let forgetuserTextFeild = ForgetPasswordTextFeild()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configfVC()
-        
+        configForgetLoginImage()
+        configforgetTxtFeild()
     }
     
     private func configfVC() {
         view.backgroundColor = .systemBackground
         navigationItem.title = "Forget Password"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(dismissVC))
+        view.addSubViews(forgetPasswordImage, forgetuserTextFeild)
     }
     
+    private func configForgetLoginImage() {
+        forgetPasswordImage.image = Images.forgetpassword_Image
+        forgetPasswordImage.contentMode = .scaleAspectFit
+        forgetPasswordImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            forgetPasswordImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            forgetPasswordImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            forgetPasswordImage.heightAnchor.constraint(equalToConstant: 350),
+            forgetPasswordImage.widthAnchor.constraint(equalToConstant: 350),
+        ])
+    }
+    
+    private func configforgetTxtFeild() {
+        
+        NSLayoutConstraint.activate([
+            forgetuserTextFeild.topAnchor.constraint(equalTo: forgetPasswordImage.bottomAnchor, constant: 20),
+            forgetuserTextFeild.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            forgetuserTextFeild.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            forgetuserTextFeild.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+    }
 
 }
