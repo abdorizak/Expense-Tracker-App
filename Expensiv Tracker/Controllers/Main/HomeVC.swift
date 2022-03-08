6385a9bf01910e0709c49c4e1f904ac2b17f4d6e
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-08 14:09:13 +0300

    Controllers - Main - HomeVC

--- Expensiv Tracker/Controllers/Main/HomeVC.swift
+++ Expensiv Tracker/Controllers/Main/HomeVC.swift
@@ -9,15 +9,26 @@ import UIKit
 
 class HomeVC: UIViewController {
     
-    private let headerView          = UIView()
-    private let balanceInfo         = CardView()
-    
+    private let headerView              = UIView()
+    private let balanceInfo             = CardView()
+    private let transectionsLabel       = CustomLabel(textAlignment: .left, fontSize: 24, textWeight: .medium, text: "Transections")
+    private let tableView: UITableView  = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        view.addSubViews(headerView, balanceInfo)
+        tableView.delegate   = self
+        tableView.dataSource = self
+        view.addSubViews(headerView, balanceInfo, transectionsLabel, tableView)
         ConfigureHeaderView()
+        configureShowBlanceView()
+        configTransectionLabel()
+        configTableView()
     }
 
 
@@ -44,14 +55,44 @@ class HomeVC: UIViewController {
             balanceInfo.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 10),
             balanceInfo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             balanceInfo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            balanceInfo.heightAnchor.constraint(equalToConstant: 220)
+            balanceInfo.heightAnchor.constraint(equalToConstant: 250)
         ])
     }
     
     
+    private func configTransectionLabel() {
+        NSLayoutConstraint.activate([
+            transectionsLabel.topAnchor.constraint(equalTo: balanceInfo.bottomAnchor, constant: 30),
+            transectionsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            transectionsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            transectionsLabel.heightAnchor.constraint(equalToConstant: 26)
+        ])
+    }
     
     
+    private func configTableView() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: transectionsLabel.bottomAnchor, constant: 10),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            
+        ])
+    }
     
+}
 
+
+extension HomeVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        return UITableViewCell()
+    }
+    
+    
 }
 
