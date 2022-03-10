4ccf1e44aff3849fcff21bf77447ffae0b95d47a
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-10 17:21:48 +0300

    ExpensivTracker.xcodeproj

---
+++ Expensiv Tracker/Custom Views/ImageView/AvatarImageView.swift
@@ -0,0 +1,20 @@
+//
+//  AvatarImageView.swift
+//  Expensiv Tracker
+//
+//  Created by Abdirizak Hassan on 3/10/22.
+//
+
+import UIKit
+
+class AvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Expensiv Tracker/Custom Views/View Controllers/HeaderVC.swift
@@ -0,0 +1,29 @@
+//
+//  HeaderVC.swift
+//  Expensiv Tracker
+//
+//  Created by Abdirizak Hassan on 3/9/22.
+//
+
+import UIKit
+
+class HeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
