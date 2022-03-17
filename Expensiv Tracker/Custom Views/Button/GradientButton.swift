6481f8354ae094b1c6ba6c2f4ab873c980eb8f6f
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-17 09:04:01 +0300

    CustomViews - Button - GradientButton

---
+++ Expensiv Tracker/Custom Views/Button/GradientButton.swift
@@ -0,0 +1,41 @@
+//
+//  GredientButton.swift
+//  Expensiv Tracker
+//
+//  Created by Abdirizak Hassan on 3/16/22.
+//
+
+import UIKit
+
+class GradientButton: UIButton {
+    
+    private let gradient = CAGradientLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        config()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func config() {
+        translatesAutoresizingMaskIntoConstraints = false
+        gradient.frame = bounds
+        gradient.colors = [
+            UIColor.systemOrange.cgColor,
+            UIColor.systemPurple.cgColor,
+            UIColor.systemTeal.cgColor,
+        ]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1 )
+        gradient.cornerRadius = 10
+        layer.addSublayer(gradient)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradient.frame = bounds
+    }
+}
