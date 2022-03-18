b822c868f71e3f805cb5f23b28cc476b888b34d4
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-18 16:12:14 +0300

    Controllers - Main - TransectionsVC

--- Expensiv Tracker/Controllers/Main/TransectionsVC.swift
+++ Expensiv Tracker/Controllers/Main/TransectionsVC.swift
@@ -8,16 +8,54 @@
 import UIKit
 
 class TransectionsVC: UIViewController {
+    
+    lazy var segment: UISegmentedControl = {
+        let items = ["Income", "Expenses"]
+        let segment = UISegmentedControl(items: items)
+        segment.selectedSegmentIndex = 0
+        let font = UIFont.systemFont(ofSize: 18)
+        segment.setTitleTextAttributes([NSAttributedString.Key.font: font], for: .normal)
+        segment.translatesAutoresizingMaskIntoConstraints = false
+        segment.addTarget(self, action: #selector(siutDidChange(_:)), for: .valueChanged)
+        segment.layer.cornerRadius = 15
+        return segment
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationItem.title = "Tractions"
-        navigationItem.largeTitleDisplayMode  = .always
-        navigationController?.navigationBar.prefersLargeTitles = true
+        configTransectionsVC()
+        configSegment()
     }
     
-
-   
-
+    
+    private func configTransectionsVC() {
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Transections"
+        navigationItem.largeTitleDisplayMode  = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.addSubViews(segment)
+    }
+    
+    @objc func siutDidChange(_ segmentController: UISegmentedControl) {
+        switch segmentController.selectedSegmentIndex {
+        case 0:
+            print("Tapped")
+        case 1:
+            print("Tapped..")
+        default: break
+            
+        }
+    }
+    
+    private func configSegment() {
+        
+        NSLayoutConstraint.activate([
+            segment.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            segment.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            segment.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            segment.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
 }
