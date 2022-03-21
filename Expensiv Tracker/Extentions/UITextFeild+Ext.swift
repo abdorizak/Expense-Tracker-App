cbef94a2423b0a0f5b417ca15b82180967e0e03f
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-21 14:37:53 +0300

    Extentions - UITextFeild+Ext

--- Expensiv Tracker/Extentions/UITextFeild+Ext.swift
+++ Expensiv Tracker/Extentions/UITextFeild+Ext.swift
@@ -5,4 +5,31 @@
 //  Created by Abdirizak Hassan on 3/21/22.
 //
 
-import Foundation
+import UIKit
+
+enum LinePosition {
+    case top
+    case bottom
+}
+
+extension UIView {
+    func addLine(position: LinePosition, color: UIColor, width: Double) {
+        let lineView = UIView()
+        lineView.backgroundColor = color
+        lineView.translatesAutoresizingMaskIntoConstraints = false // This is important!
+        self.addSubview(lineView)
+
+        let metrics = ["width" : NSNumber(value: width)]
+        let views = ["lineView" : lineView]
+        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[lineView]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+
+        switch position {
+        case .top:
+            self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[lineView(width)]", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+            break
+        case .bottom:
+            self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[lineView(width)]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics:metrics, views:views))
+            break
+        }
+    }
+}
