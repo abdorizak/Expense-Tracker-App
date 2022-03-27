7cffa085fe07aadba5b05dd3e770ddc7936502e0
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-27 18:10:37 +0300

    Controllers - Main - ProfileVC

---
+++ Expensiv Tracker/Controllers/Main/ProfileVC.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileVC.swift
+//  Expensiv Tracker
+//
+//  Created by Abdirizak Hassan on 3/27/22.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
