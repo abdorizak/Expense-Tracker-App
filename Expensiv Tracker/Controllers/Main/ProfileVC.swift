012275e7244942fc79670e71a5975c119670c516
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-03-27 20:14:03 +0300

    Controllers - Main - ProfileVC

--- Expensiv Tracker/Controllers/Main/ProfileVC.swift
+++ Expensiv Tracker/Controllers/Main/ProfileVC.swift
@@ -7,20 +7,78 @@
 
 import UIKit
 
-class ProfileVC: UIViewController {
+class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let test = ["Item 1", "Item 1", "Item 1", "Item 1", "Item 1"]
+    
     // MARK: - TableView
     private let tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .insetGrouped)
         tableView.translatesAutoresizingMaskIntoConstraints = false
-//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        configProfileVC()
+        configTableView()
     }
     
+    private func configProfileVC() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        navigationItem.title = "Profile"
+        
+    }
+    
+    private func configTableView() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "View"
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+            return 90
+        }
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+            guard let header = view as? UITableViewHeaderFooterView else { return }
+            
+//            header.backgroundColor = UIColor.red
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .regular)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y + 20, width: 100, height: header.bounds.height)
+        header.textLabel?.textColor = .label
+        header.textLabel?.text = "HI"
+        }
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let vw = UIView()
+//        vw.backgroundColor = UIColor.tertiarySystemBackground
+//
+//        return vw
+//    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        test.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = test[indexPath.row]
+        return cell
+    }
 
 }
