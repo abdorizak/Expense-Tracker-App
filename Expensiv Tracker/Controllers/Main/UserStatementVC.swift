85cbbb4f70578deead99aff30848c5b10647b5f8
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-04-19 10:01:09 +0300

    Controllers - Main - UserStatementVC.swift

---
+++ Expensiv Tracker/Controllers/Main/UserStatementVC.swift
@@ -0,0 +1,16 @@
+//
+//  UserStatementVC.swift
+//  Expensiv Tracker
+//
+//  Created by Abdirizak Hassan on 4/19/22.
+//
+
+import UIKit
+
+class UserStatementVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
