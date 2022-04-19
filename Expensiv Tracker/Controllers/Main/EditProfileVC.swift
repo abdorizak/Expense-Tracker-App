3ff2cab00effffa362c1b9e1d36fc465f0ee1b22
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-04-19 10:01:37 +0300

    Controllers - Main - EditProfileVC

---
+++ Expensiv Tracker/Controllers/Main/EditProfileVC.swift
@@ -0,0 +1,16 @@
+//
+//  EditProfileVC.swift
+//  Expensiv Tracker
+//
+//  Created by Abdirizak Hassan on 4/19/22.
+//
+
+import UIKit
+
+class EditProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackgroundu
+    }
+}
