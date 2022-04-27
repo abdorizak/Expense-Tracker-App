47c0ea689e6a963a059230f80dfb35e36d7e1d0c
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-04-27 15:05:00 +0300

    Models - Transection

---
+++ Expensiv Tracker/Models/Transection.swift
@@ -0,0 +1,14 @@
+//
+//  Transection.swift
+//  Expensiv Tracker
+//
+//  Created by Abdirizak Hassan on 4/24/22.
+//
+
+import Foundation
+
+struct Transections {
+    let description: String
+    let ammount: Int
+    let date: String
+}
