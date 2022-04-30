bdde99f23ada7ed17225762f68f0ca30f614417f
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-04-30 14:15:20 +0300

    Controllers - ProfileViews - EditProfileView

---
+++ Expensiv Tracker/Controllers/ProfileViews/EditProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EditProfileViewController.swift
+//  Expensiv Tracker
+//
+//  Created by Abdirizak Hassan on 4/29/22.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+
+}
