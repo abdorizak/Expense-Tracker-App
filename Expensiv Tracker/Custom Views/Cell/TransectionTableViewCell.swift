64f8b5d026e75c86d9660ea7c7704b6bbea636ad
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-05-11 21:52:53 +0300

    CustomView - Cell - TransectionTableViewCell

--- Expensiv Tracker/Custom Views/Cell/TransectionTableViewCell.swift
+++ Expensiv Tracker/Custom Views/Cell/TransectionTableViewCell.swift
@@ -11,12 +11,22 @@ class TransectionTableViewCell: UITableViewCell {
     
     static let identifier = String(describing: TransectionTableViewCell.self)
     
+    let cardView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = .zero
+        view.layer.cornerRadius = 15
+        view.layer.shadowOpacity = 0.1
+        view.layer.shadowRadius = 10
+        return view
+    }()
+    let iconImage = AvatarImageView(frame: .zero)
     let titleLabel = CustomLabel(textAlignment: .left, fontSize: 20, textWeight: .heavy, text: "Hayaat Market")
     let descriptionLabel = CustomLabel(textAlignment: .left, fontSize: 18, textWeight: .heavy, text: "Waxaa soo Gatay 3 Shaati")
     let AmountLabel = CustomLabel(textAlignment: .left, fontSize: 18, textWeight: .heavy, text: "$200")
     let dateLabel = CustomLabel(textAlignment: .left, fontSize: 18, textWeight: .heavy, text: "04/04/2022")
-    
-    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -28,15 +38,26 @@ class TransectionTableViewCell: UITableViewCell {
     }
     
     func config() {
-        contentView.addSubViews(titleLabel)
+        iconImage.image = Images.spending_Money
+        iconImage.layer.cornerRadius = iconImage.frame.width / 2
+        contentView.addSubview(cardView)
+        
         
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
-            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
-            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
-            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 20)
+            cardView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            cardView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -14),
+            cardView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 14),
+            cardView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 10),
+            
+            
+
         ])
-        
+    }
+    
+    override func layoutSubviews() {
+         super.layoutSubviews()
+         let bottomSpace: CGFloat = 10.0
+         contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: bottomSpace, right: 0))
     }
     
 }
