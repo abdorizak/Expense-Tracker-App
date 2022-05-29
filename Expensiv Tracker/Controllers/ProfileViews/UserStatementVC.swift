1198e895bb1bf6e6ce31159366461c6783ffaedd
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-05-29 18:57:58 +0300

    Controllers - ProfileViews - UserStatementVC

--- Expensiv Tracker/Controllers/ProfileViews/UserStatementVC.swift
+++ Expensiv Tracker/Controllers/ProfileViews/UserStatementVC.swift
@@ -9,10 +9,50 @@ import UIKit
 
 class UserStatementVC: UIViewController {
     
-    
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.register(TransectionTableViewCell.self, forCellReuseIdentifier: TransectionTableViewCell.identifier)
+        table.showsVerticalScrollIndicator = false
+        table.translatesAutoresizingMaskIntoConstraints = false
+//        table.separatorStyle = .none
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configTableView()
     }
+    
+    func configTableView() {
+        view.addSubview(tableView)
+        tableView.delegate      = self
+        tableView.dataSource    = self
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+}
+
+
+extension UserStatementVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        90
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TransectionTableViewCell.identifier, for: indexPath) as! TransectionTableViewCell
+        cell.selectionStyle = .none
+        return cell
+    }
+ 
 }
