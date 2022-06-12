7a9284616550dab4a7a6e120baab43670f85ad44
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-06-12 23:18:48 +0300

    Controllers - ProfileViews - AboutUsVC

--- Expensiv Tracker/Controllers/ProfileViews/AboutUsVC.swift
+++ Expensiv Tracker/Controllers/ProfileViews/AboutUsVC.swift
@@ -8,11 +8,38 @@
 import UIKit
 
 class AboutUsVC: UIViewController {
+    
+    private let expense_Img  = AvatarImageView(frame: .zero)
+    private let expense_Label = CustomLabel(textAlignment: .center, fontSize: 35, textWeight: .bold)
+    
+    private let expense_body: UITextView = {
+        let textView = UITextView()
+        textView.isScrollEnabled = false
+        textView.allowsEditingTextAttributes = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configAboutUS()
+    }
+    
+    private func configAboutUS() {
+        view.addSubViews(expense_Img, expense_Label)
+        expense_Img.image = Images.aboutUS_ExpenseImage
+        expense_Img.contentMode = .scaleAspectFill
+        NSLayoutConstraint.activate([
+            expense_Img.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            expense_Img.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            expense_Img.heightAnchor.constraint(equalToConstant: 300),
+            expense_Img.widthAnchor.constraint(equalToConstant: 300)
+            
+        ])
     }
 
+    
+    
 
 }
