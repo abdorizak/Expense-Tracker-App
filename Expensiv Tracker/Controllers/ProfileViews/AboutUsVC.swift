4d1c8e5471b0261ec20965c02515eb78e5090b17
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-06-13 11:30:49 +0300

    Controllers - ProfileViews - AboutUsVC

--- Expensiv Tracker/Controllers/ProfileViews/AboutUsVC.swift
+++ Expensiv Tracker/Controllers/ProfileViews/AboutUsVC.swift
@@ -10,12 +10,11 @@ import UIKit
 class AboutUsVC: UIViewController {
     
     private let expense_Img  = AvatarImageView(frame: .zero)
-    private let expense_Label = CustomLabel(textAlignment: .center, fontSize: 38, textWeight: .bold)
     
     private let expense_description: UITextView = {
         let textView = UITextView()
         let attributeText = NSMutableAttributedString(string: "Expense Tracker App", attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 36), NSAttributedString.Key.foregroundColor: UIColor.label])
-        attributeText.append(NSAttributedString(string: "\n\n An expense is the cost of operations that a company incurs to generate revenue. As the popular saying goes, “it costs money to make money.Common expenses include payments to suppliers, employee wages, factory leases, and equipment depreciation. Businesses are allowed to write off tax-deductible expenses on their income tax returns to lower their taxable income and thus their tax liability. However, the Internal Revenue Service (IRS) has strict rules on which expenses businesses are allowed to claim as a deduction.", attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray]))
+        attributeText.append(NSAttributedString(string: "\n\n An expense is the cost of operations that a company incurs to generate revenue. As the popular saying goes, “it costs money to make money.Common expenses include payments to suppliers, employee wages, factory leases, and equipment depreciation. Businesses are allowed to write off tax-deductible expenses on their income tax returns to lower their taxable income and thus their tax liability. However, the Internal Revenue Service (IRS) has strict rules on which expenses businesses are allowed to claim as a deduction. An expense is the cost of operations that a company incurs to generate revenue. As the popular saying goes, “it costs money to make money.Common expenses include payments to suppliers, employee wages, factory leases, and equipment depreciation. Businesses are allowed to write off tax-deductible expenses on their income tax returns to lower their taxable income and thus their tax liability. However, the Internal Revenue Service (IRS) has strict rules on which expenses businesses are allowed to claim as a deduction.", attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray]))
         textView.attributedText = attributeText
         textView.isScrollEnabled = false
         textView.isEditable = false
@@ -43,7 +42,7 @@ class AboutUsVC: UIViewController {
             expense_description.topAnchor.constraint(equalTo: expense_Img.bottomAnchor, constant: 30),
             expense_description.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             expense_description.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            expense_description.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 30),
+            expense_description.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 20),
             
             
             
