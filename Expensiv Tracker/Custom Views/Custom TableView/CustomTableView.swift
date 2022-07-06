1582b5d34af3fa2f4ccb52996f6478c3ed611be4
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-07-06 12:59:46 +0300

    CustomViews - CustomTableView

--- Expensiv Tracker/Custom Views/Custom TableView/CustomTableView.swift
+++ Expensiv Tracker/Custom Views/Custom TableView/CustomTableView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CustomTableViewController: UITableView {
+class CustomTableView: UITableView {
     
     override init(frame: CGRect, style: UITableView.Style) {
         super.init(frame: frame, style: style)
