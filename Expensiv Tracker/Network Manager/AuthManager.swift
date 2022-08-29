d04efdb56a32794cc6e1447c912a9fd39a0298f0
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-08-29 15:25:53 +0300

    Network Manager --AuthManager --added loginFunc and SingOut

--- Expensiv Tracker/Network Manager/AuthManager.swift
+++ Expensiv Tracker/Network Manager/AuthManager.swift
@@ -9,64 +9,87 @@
 import UIKit
 
 final class AuthManager {
+    static let shared   = AuthManager()
+    private let decoder         = JSONDecoder()
     
-    static let shared = AuthManager()
-    
-    private var user: User?
-    
-    private init() {}
+    // init
+    private init() { }
     
     var isSingedIn: Bool {
-        return true
+        return accessToken != nil
     }
     
     private var accessToken: String? {
         return UserDefaults.standard.string(forKey: "access_token")
     }
     
-    func withValidate(completion: @escaping (String) -> Void) {
-        if let token = accessToken {
-            completion(token)
+    func withValidate() async throws -> String {
+        guard let token = accessToken else {
+            throw ExError.NotFound
         }
+        return token
     }
     
-    
-    func login(username: String, password: String, completion: @escaping (Result<LoginResponse, ExError>) -> Void) {
-        guard let url = URL(string: URL_S.login_URL) else {
-            completion(.failure(.invalidURL))
-            return
+    func login(username: String, password: String) async throws -> LoginResponse {
+        guard let url = URL(string: API.baseURL + "auth/login") else {
+            throw ExError.invalidURL
         }
         
-        let body = LoginBody(username: username, password: username)
+        let body = LoginBody(username: username, password: password)
         
         var request = URLRequest(url: url)
+        request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = try? JSONEncoder().encode(body)
         
-        URLSession.shared.dataTask(with: request) { data, res, error in
-            
-            guard let data = data, error == nil else {
-                completion(.failure(.inValidData))
-                return
-            }
-            
-            guard let httpResponse = res as? HTTPURLResponse, httpResponse.statusCode == 200
-            else {
-                completion(.failure(.httpResponse))
-                return
-            }
-            
-            do {
-                let login_response = try JSONDecoder().decode(LoginResponse.self, from: data)
-                completion(.success(login_response))
-            } catch {
-                completion(.failure(.invalidCredentials))
-            }
-        }.resume()
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw ExError.invalidResponse }
+        
+        do {
+            let res = try decoder.decode(LoginResponse.self, from: data)
+            catchToken(result: res)
+            return res
+        } catch {
+            throw ExError.inValidData
+        }
     }
     
-    private func catchToken(result: LoginResponse) {
-        UserDefaults.standard.setValue(result.access_token, forKey: "access_token")
+    func login(with pin: Int) async throws -> LoginResponse {
+        guard let url = URL(string: API.baseURL + "auth/pin") else {
+            throw ExError.invalidURL
+        }
+        
+        let body = Pin(pin: pin)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(body)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw ExError.invalidResponse }
+        do {
+            let res = try decoder.decode(LoginResponse.self, from: data)
+            catchToken(result: res)
+            return res
+        } catch {
+            throw ExError.inValidData
+        }
     }
+    
+    
+    private func catchToken(result: LoginResponse) {
+        UserDefaults.standard.setValue(result.accessToken, forKey: "access_token")
+        UserDefaults.standard.setValue(result.userInfo, forKey: "user_id")
+    }
+    
+    func SingOut() -> Bool {
+        UserDefaults.standard.setValue(nil, forKey: "access_token")
+        return true
+    }
+    
+    
     
 }
