3f5e239861fcb2c5fc2beeaa35e041ee38fc1352
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-08-29 15:28:14 +0300

    Utilits --Constants -added enum for API endPoint

--- Expensiv Tracker/Utilits/Constants.swift
+++ Expensiv Tracker/Utilits/Constants.swift
@@ -8,11 +8,9 @@
 
 import UIKit
 
-struct URL_S {
-    static let login_URL = "http://localhost:4400/api/auth/login"
+struct API {
+    static let baseURL = "http://localhost:4400/api/v1/"
 }
-
-
 enum Images {
     static let placeHolderImage      = UIImage(named: "default")
     static let aboutUS_ExpenseImage  = UIImage(named: "Expense_Backgroud")
@@ -30,6 +28,7 @@ enum Images {
     static let logout                = UIImage(systemName: "rectangle.portrait.and.arrow.right.fill")
     
     static let spending_Money        = UIImage(named: "spending_money")
+    static let login_pin_background  = UIImage(named: "login_withPin_img")
 }
 
 
@@ -45,7 +44,7 @@ enum DeviceTypes {
     static let idiom                    = UIDevice.current.userInterfaceIdiom
     static let nativeScale              = UIScreen.main.nativeScale
     static let scale                    = UIScreen.main.scale
-
+    
     static let isiPhoneSE               = idiom == .phone && ScreenSize.maxLength == 568.0
     static let isiPhone8Standard        = idiom == .phone && ScreenSize.maxLength == 667.0 && nativeScale == scale
     static let isiPhone8Zoomed          = idiom == .phone && ScreenSize.maxLength == 667.0 && nativeScale > scale
@@ -54,7 +53,7 @@ enum DeviceTypes {
     static let isiPhoneX                = idiom == .phone && ScreenSize.maxLength == 812.0
     static let isiPhoneXsMaxAndXr       = idiom == .phone && ScreenSize.maxLength == 896.0
     static let isiPad                   = idiom == .pad && ScreenSize.maxLength >= 1024.0
-
+    
     static func isiPhoneXAspectRatio() -> Bool {
         return isiPhoneX || isiPhoneXsMaxAndXr
     }
