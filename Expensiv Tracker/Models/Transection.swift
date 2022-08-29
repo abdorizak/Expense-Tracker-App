4794bc32d884253ace757319134164d3fbf733ff
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-08-29 15:24:12 +0300

    Models --Transection --Arranged

--- Expensiv Tracker/Models/Transection.swift
+++ Expensiv Tracker/Models/Transection.swift
@@ -14,3 +14,41 @@ struct Transections {
     let type: String
     let ammount: Double
 }
+
+struct Transactions: Decodable {
+    let status: Int
+    let transaction: [Transaction]
+}
+
+struct Transaction: Decodable {
+    let id: String
+    let userID: String
+    let title: String
+    let type: String
+    let description: String
+    let amount: Double
+    let date: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case userID
+        case title
+        case type
+        case description
+        case amount
+        case date
+    }
+}
+
+struct TransactionBody: Codable {
+    let userID: String
+    let type: String
+    let title: String
+    let description: String
+    let amount: Double
+}
+
+struct TransactionReponse: Decodable {
+    let message: String?
+    let status: Int?
+}
