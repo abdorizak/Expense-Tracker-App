ccdabfc7aa8f4669dd0ee2bfb47d9aabf3d40f1e
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-09-04 11:23:34 +0300

    Utilits --Constants -Fixed IPhone SE ScreenSize

--- Expensiv Tracker/Utilits/Constants.swift
+++ Expensiv Tracker/Utilits/Constants.swift
@@ -31,30 +31,29 @@ enum Images {
     static let login_pin_background  = UIImage(named: "login_withPin_img")
 }
 
-
-enum ScreenSize {
-    static let width        = UIScreen.main.bounds.size.width
-    static let height       = UIScreen.main.bounds.size.height
-    static let maxLength    = max(ScreenSize.width, ScreenSize.height)
-    static let minLength    = min(ScreenSize.width, ScreenSize.height)
-}
-
-
 enum DeviceTypes {
+    enum ScreenSize {
+        static let width                = UIScreen.main.bounds.size.width
+        static let height               = UIScreen.main.bounds.size.height
+        static let maxLength            = max(ScreenSize.width, ScreenSize.height)
+        static let minLength            = min(ScreenSize.width, ScreenSize.height)
+    }
+    
     static let idiom                    = UIDevice.current.userInterfaceIdiom
     static let nativeScale              = UIScreen.main.nativeScale
     static let scale                    = UIScreen.main.scale
-    
-    static let isiPhoneSE               = idiom == .phone && ScreenSize.maxLength == 568.0
+
+    static let isiPhoneSE               = idiom == .phone && ScreenSize.maxLength == 667.0
     static let isiPhone8Standard        = idiom == .phone && ScreenSize.maxLength == 667.0 && nativeScale == scale
     static let isiPhone8Zoomed          = idiom == .phone && ScreenSize.maxLength == 667.0 && nativeScale > scale
     static let isiPhone8PlusStandard    = idiom == .phone && ScreenSize.maxLength == 736.0
-    static let isiPhone8PlusZoomed      = idiom == .phone && ScreenSize.maxLength == 736.0 && nativeScale < scale
+    static let isiPhone8PlusZoomed      = idiom == .phone && ScreenSize.maxLength == 736.0 && nativeScale > scale
     static let isiPhoneX                = idiom == .phone && ScreenSize.maxLength == 812.0
     static let isiPhoneXsMaxAndXr       = idiom == .phone && ScreenSize.maxLength == 896.0
     static let isiPad                   = idiom == .pad && ScreenSize.maxLength >= 1024.0
-    
+
     static func isiPhoneXAspectRatio() -> Bool {
         return isiPhoneX || isiPhoneXsMaxAndXr
     }
 }
+
