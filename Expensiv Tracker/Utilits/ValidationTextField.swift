95fe27b883302e4c380de924ec46d00d6a1f6cf7
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-09-04 11:46:40 +0300

    Utilits -ValidationTextField

--- Expensiv Tracker/Utilits/ValidationTextField.swift
+++ Expensiv Tracker/Utilits/ValidationTextField.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+final class Validation {
+    static let validate = Validation()
+    
+    func validateName(name: String) ->Bool {
+        // Length be 18 characters max and 3 characters minimum, you can always modify.
+        let nameRegex = "^\\w{3,18}$"
+        let trimmedString = name.trimmingCharacters(in: .whitespaces)
+        let validateName = NSPredicate(format: "SELF MATCHES %@", nameRegex)
+        let isValidateName = validateName.evaluate(with: trimmedString)
+        return isValidateName
+    }
+    
+    func validaPhoneNumber(phoneNumber: String) -> Bool {
+        let phoneNumberRegex = "^[6-9]\\d{9}$"
+        let trimmedString = phoneNumber.trimmingCharacters(in: .whitespaces)
+        let validatePhone = NSPredicate(format: "SELF MATCHES %@", phoneNumberRegex)
+        let isValidPhone = validatePhone.evaluate(with: trimmedString)
+        return isValidPhone
+    }
+    
+    func validateEmailId(emailID: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let trimmedString = emailID.trimmingCharacters(in: .whitespaces)
+        let validateEmail = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let isValidateEmail = validateEmail.evaluate(with: trimmedString)
+        return isValidateEmail
+    }
+    
+    func validatePassword(password: String) -> Bool {
+        //Minimum 8 characters at least 1 Alphabet and 1 Number:
+        let passRegEx = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
+        let trimmedString = password.trimmingCharacters(in: .whitespaces)
+        let validatePassord = NSPredicate(format:"SELF MATCHES %@", passRegEx)
+        let isvalidatePass = validatePassord.evaluate(with: trimmedString)
+        return isvalidatePass
+    }
+    
+    func username(username: String) -> Bool {
+        let otherRegexString = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{4,}$"
+        let trimmedString = username.trimmingCharacters(in: .whitespaces)
+        let validateOtherString = NSPredicate(format: "SELF MATCHES %@", otherRegexString)
+        let isValidateOtherString = validateOtherString.evaluate(with: trimmedString)
+        return isValidateOtherString
+    }
+    
+}
