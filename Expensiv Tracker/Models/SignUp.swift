b0be0d945b80ba11aea3c0c7a915ef9d2148cb9e
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-09-13 10:46:20 +0300

    Models -SignUP Model

--- Expensiv Tracker/Models/SignUp.swift
+++ Expensiv Tracker/Models/SignUp.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+
+struct SignUp: Codable {
+    let name: String
+    let email: String
+    let phone: String
+    let username: String
+    let password: String
+    let pin: Int
+}
+
+
+struct SignUpResponse: Decodable {
+    let status: Int?
+    let message: String?
+}
