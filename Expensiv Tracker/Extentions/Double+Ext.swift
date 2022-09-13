361d86c8c131a5d03b298df32e2236c0ecb5e049
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2022-09-13 10:39:40 +0300

    added Formatt method Fixed issue #4

--- Expensiv Tracker/Extentions/Double+Ext.swift
+++ Expensiv Tracker/Extentions/Double+Ext.swift
@@ -20,4 +20,40 @@ extension Double {
     var formatDouble: String {
         return String(format: "%.01f", self)
     }
+    
+    func reduceScale(to places: Int) -> Double {
+        let multiplier = pow(10, Double(places))
+        let newDecimal = multiplier * self // move the decimal right
+        let truncated = Double(Int(newDecimal)) // drop the fraction
+        let originalDecimal = truncated / multiplier // move the decimal back
+        return originalDecimal
+    }
+    
+    func formatNumber() -> String {
+        let num = abs(Double(self))
+        let sign = (self < 0) ? "-" : ""
+        
+        switch num {
+        case 1_000_000_000...:
+            var formatted = num / 1_000_000_000
+            formatted = formatted.reduceScale(to: 1)
+            return "\(sign)\(formatted)B"
+            
+        case 1_000_000...:
+            var formatted = num / 1_000_000
+            formatted = formatted.reduceScale(to: 1)
+            return "\(sign)\(formatted)M"
+            
+        case 1_000...:
+            var formatted = num / 1_000
+            formatted = formatted.reduceScale(to: 1)
+            return "\(sign)\(formatted)K"
+            
+        case 0...:
+            return "\(self)"
+            
+        default:
+            return "\(sign)\(self)"
+        }
+    }
 }
