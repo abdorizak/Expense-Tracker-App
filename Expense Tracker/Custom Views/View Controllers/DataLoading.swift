caeb3da4832eb14821c94cf7df96ed86662c9784
Abdirizak Abdalla  <cabdirizaaqyare12@gmail.com>
2024-02-22 11:39:32 +0300

    Refactored DataLoadingVC: added showLoadingView and dismissLoadingView for better UX.

--- Expense Tracker/Custom Views/View Controllers/DataLoading.swift
+++ Expense Tracker/Custom Views/View Controllers/DataLoading.swift
@@ -9,4 +9,60 @@
 // Website: https://abdorizak.dev
 
 
-import Foundation
+import UIKit
+
+class DataLoadingVC: UIViewController {
+    
+    var containerView: UIView!
+    var loadingView: UIView!
+
+
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor   = .systemBackground
+        containerView.alpha             = 0
+        
+        UIView.animate(withDuration: 0.25) { self.containerView.alpha = 0.8 }
+        
+        // Create a rectangular loading view
+        loadingView = UIView()
+        loadingView.backgroundColor = .secondarySystemBackground
+        loadingView.layer.cornerRadius = 15
+        loadingView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(loadingView)
+        
+        // Set constraints for the loading view
+        NSLayoutConstraint.activate([
+            loadingView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            loadingView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            loadingView.widthAnchor.constraint(equalToConstant: 90),
+            loadingView.heightAnchor.constraint(equalToConstant: 90)
+        ])
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        loadingView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: loadingView.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: loadingView.centerXAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            if let containerView = self.containerView {
+                containerView.removeFromSuperview()
+                self.containerView = nil
+            }
+        }
+    }
+
+    
+}
