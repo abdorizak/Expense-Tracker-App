a59626761c9f6f6887e85b65ffb4858dbc033942
Abdirizak Abdalla  <cabdirizaaqyare12@gmail.com>
2024-02-22 11:30:55 +0300

    Added CharView.swift and DataLoading.swift to project; improving chart rendering.

---
+++ Expense Tracker/Custom Views/SwiftUI CharView/CharView.swift
@@ -0,0 +1,22 @@
+//
+// CharView.swift
+// Expense Tracker
+//
+// Created by Abdirizak Hassan on 2/17/24. 💻
+// Copyright © 2024 Abdirizak Hassan DR X. All Rights Reserved.
+//
+// GitHub: https://github.com/abdorizak
+// Website: https://abdorizak.dev
+
+
+import SwiftUI
+
+struct CharView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharView()
+}
---
+++ Expense Tracker/Custom Views/View Controllers/DataLoading.swift
@@ -0,0 +1,12 @@
+//
+// DataLoading.swift
+// Expense Tracker
+//
+// Created by Abdirizak Hassan on 10/22/23. 💻
+// Copyright © 2023 Abdirizak Hassan DR X. All Rights Reserved.
+//
+// GitHub: https://github.com/abdorizak
+// Website: https://abdorizak.dev
+
+
+import Foundation
