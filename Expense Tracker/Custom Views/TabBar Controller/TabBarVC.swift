8bf2c6a403654064b79164009f35beb876edc584
Abdirizak Abdalla  <cabdirizaaqyare12@gmail.com>
2024-02-22 11:37:57 +0300

    Refactored authenticating Face ID on TabBarVC for added security measures.

--- Expense Tracker/Custom Views/TabBar Controller/TabBarVC.swift
+++ Expense Tracker/Custom Views/TabBar Controller/TabBarVC.swift
@@ -2,18 +2,57 @@
 //  TabBarVC.swift
 //  Expensiv Tracker
 //
-//  Created by Abdirizak Hassan on 3/1/22.
+//  Created by Abdirizak Hassan on 3/1/22.💻
+// Copyright © 2023 Abdirizak Hassan DR X. All Rights Reserved.
 //
-// UI DESING
+// GitHub: https://github.com/abdorizak
+// Website: https://abdorizak.dev
 
 import UIKit
+import LocalAuthentication
 
 class TabBarVC: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTabBar()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        authenticateUser()
+    }
+    
+    private func authenticateUser() {
+        let isFaceIDEnabled = UserDefaults.standard.bool(forKey: "isFaceIDEnabled")
+        
+        debugPrint("Checking.. \(isFaceIDEnabled)")
+        // Only proceed with authentication if Face ID is enabled in settings
+        if isFaceIDEnabled {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate to access the app."
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
+                    DispatchQueue.main.async {
+                        if !success {
+                            // Handle authentication failure
+                            let alert = UIAlertController(title: "Authentication Failed", message: "Please try again.", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                            self?.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                }
+            } else {
+                // Face ID is not available or not enabled
+                print("Face ID is not available or not enabled.")
+            }
+        } else {
+            print("Face ID authentication is disabled in settings.")
+        }
+    }
+    
     
     private func configureTabBar() {
         view.backgroundColor = .systemBackground
@@ -45,5 +84,5 @@ class TabBarVC: UITabBarController {
         return UINavigationController(rootViewController: tranVC)
         
     }
-
+    
 }
